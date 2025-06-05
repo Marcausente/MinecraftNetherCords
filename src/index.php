653cdc8a3297f9fffee2057fd39faae1385ae05c
Marc Fernández <marcausente@gmail.com>
2025-06-05 20:28:57 +0200

    FEAT: Recibido de datos con PHP y procesado

--- src/index.php
+++ src/index.php
@@ -1,3 +1,54 @@
+<?php
+
+$x = 0;
+$y = 0;
+$z = 0;
+$result = null
+$dimension = "";
+
+if (isset($_POST['x'])) {
+    $x = (int)$_POST['x'];
+}
+if (isset($_POST['y'])) {
+    $y = (int)$_POST['y'];
+}
+if (isset($_POST['z'])) {
+    $z = (int)$_POST['z'];
+}
+if (isset($_POST['z'])) {
+    $dimension = $_POST['dimension'];
+}
+
+if ($dimension == 'overworld'){
+   $result[] = overworld_a_nether($x, $y, $z);
+
+} else if($dimension == 'nether'){
+    $result[] = nether_a_overword($x, $y, $z);
+}
+function overworld_a_nether($x, $y, $z)
+{
+    $x = $x / 8;
+    $result[0] = $x;
+    $result[1] = $y;
+    $z = $z / 8;
+    $result[2] = $z;
+    return $result;
+}
+
+function nether_a_overword($x, $y, $z)
+{
+    $x = $x * 8;
+    $result[0] = $x;
+    $result[1] = $y;
+    $z = $z * 8;
+    $result[2] = $z;
+    return $result;
+}
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
